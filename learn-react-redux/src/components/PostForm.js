a861bc7b0f95a8ddce7039acd8b067061e79b9d7
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-17 18:33:22 +0100

    Add post fetch() to send post data

--- learn-react-redux/src/components/PostForm.js
+++ learn-react-redux/src/components/PostForm.js
@@ -9,17 +9,37 @@ class PostForm extends Component {
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
         this.setState({[e.target.name]: e.target.value})
     }
 
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        };
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
     render() {
         return (
             <div>
                 <h1>ADD POST</h1>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>Title: </label><br/>
                         <input name="title" onChange={this.onChange} type="text" value={this.state.title}/>

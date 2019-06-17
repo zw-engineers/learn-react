c8a27683b512eea2eb2f8a92b8240dfb3948588d
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-17 06:20:02 +0100

    add Posts component

--- learn-react-redux/src/App.js
+++ learn-react-redux/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from "./components/Posts";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Posts/>
     </div>
   );
 }

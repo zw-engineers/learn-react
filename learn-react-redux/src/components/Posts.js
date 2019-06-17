7512650cc964e8b9bf25b0a005b2ab28684dd132
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-17 06:27:08 +0100

    add response data to component state

--- learn-react-redux/src/components/Posts.js
+++ learn-react-redux/src/components/Posts.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 
 class Posts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+
     componentWillMount() {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => this.setState({posts: data}))
     }
 
     render() {

e09835224561b0f0460e71495f16ef0cbeaf6799
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-17 06:40:18 +0100

    add PostForm to App

---
+++ learn-react-redux/src/components/PostForm.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>ADD POST</h1>
+            </div>
+        );
+    }
+}
+
+export default PostForm;

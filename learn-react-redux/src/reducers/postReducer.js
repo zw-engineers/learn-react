9bc8002e95b72502af6102148706c1a8cd447962
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-20 15:27:50 +0100

    Add default state to postReducer

---
+++ learn-react-redux/src/reducers/postReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    items: [],
+    item: {}
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+      default:
+          return state;
+  }
+}

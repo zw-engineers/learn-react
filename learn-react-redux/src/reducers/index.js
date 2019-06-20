1d94460efdee9a97ca1ed050fdad063ae3261f12
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-20 16:00:30 +0100

    Amend reducers index.js

--- learn-react-redux/src/reducers/index.js
+++ learn-react-redux/src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from "redux";
+import {postReducer} from "./postReducer";
 
 combineReducers({
     posts: postReducer
--- learn-react-redux/src/reducers/postReducer.js
+++ learn-react-redux/src/reducers/postReducer.js
@@ -1,10 +1,17 @@
+import {FETCH_POSTS} from "../actions/types";
+
 const initialState = {
     items: [],
     item: {}
 };
 
-export default function (state = initialState, action) {
+export function postReducer (state = initialState, action) {
   switch (action.type) {
+      case FETCH_POSTS:
+          return {
+              ...state,
+              items: action.payload
+          };
       default:
           return state;
   }
